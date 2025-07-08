8d5f7b6d2b989b73831d4a9fcded4cdf937346b1
marudharmertaroad <marudharmertaroad77@gmail.com>
2025-07-08 22:32:52 +0530

    Updated StudentPortal.tsx

--- src/components/StudentPortal.tsx
+++ src/components/StudentPortal.tsx
@@ -178,8 +178,7 @@ const StudentPortal: React.FC = () => {
         
         <main className="flex-1 p-6 md:ml-0">
           <div className="max-w-7xl mx-auto">
-            {renderContent()}
-          </div>
+{!loading && !error && renderContent()}          </div>
         </main>
       </div>
 
