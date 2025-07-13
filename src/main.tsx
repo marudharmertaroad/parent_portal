9dc4206fa1462e64e196f769b98a9cc0d400f682
marudharmertaroad <marudharmertaroad77@gmail.com>
2025-07-13 23:49:16 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import './lib/firebase.ts';
+import './utils/firebase-init.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
