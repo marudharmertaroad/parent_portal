411f3216f4799d0b4b3ec4697f8f97de561c6637
marudharmertaroad <marudharmertaroad77@gmail.com>
2025-07-18 18:08:37 +0530

    Updated firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,10 +1,10 @@
-// In public/firebase-messaging-sw.js
+// public/firebase-messaging-sw.js
 
-// These import statements are required.
+// These import scripts are required for the service worker to function.
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
-// --- PASTE YOUR FIREBASE CONFIG OBJECT HERE ---
+// --- PASTE THE SAME CONFIG OBJECT FROM STEP 1 HERE ---
 const firebaseConfig = {
   apiKey: "AIzaSyAB6luds9Z-Luc_T4Of4qNQrRe9Oh7Pt8Q",
   authDomain: "marudhar-school-erp.firebaseapp.com",
@@ -13,21 +13,20 @@ const firebaseConfig = {
   messagingSenderId: "400371137837",
   appId: "1:400371137837:web:c8a7bbed749f2d1029a7a4"
 };
-// ---------------------------------------------------
 
-// Initialize the Firebase app in the service worker
+// ----------------------------------------------------
+
 firebase.initializeApp(firebaseConfig);
-
-// Retrieve an instance of Firebase Messaging so that it can handle background messages.
 const messaging = firebase.messaging();
 
+// This part handles notifications that arrive when the app is in the background.
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  
+
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/logo.png' // Or any other icon in your public folder
+    icon: '/logo.png' // You can change this to your school's logo if it's in the public folder
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
