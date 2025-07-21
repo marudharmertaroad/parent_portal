83f3f525b006c5df20dfcce1e1993de00a20c5f2
marudharmertaroad <marudharmertaroad77@gmail.com>
2025-07-21 21:15:11 +0530

    Added capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'in.mdsmertaroad.parentapp',
+  appName: 'MDS ERP',
+  webDir: 'dist'
+};
+
+export default config;
